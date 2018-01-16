7138b0324c528d35116e9d87b1bc0dee20a62596
Daniel West <daniel@silverback.is>
2018-01-16 13:41:52 +0000

    Define services

---
+++ src/DependencyInjection/ApiComponentExtension.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Silverback\ApiComponentBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
+
+class ApiComponentExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $this->loadServiceConfig($container);
+    }
+
+    private function loadServiceConfig(ContainerBuilder $container)
+    {
+        $loader = new PhpFileLoader(
+            $container,
+            new FileLocator(__DIR__.'/../Resources/config')
+        );
+        $loader->load('services.php');
+    }
+}
---
+++ src/Resources/config/services.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Silverback\ApiComponentBundle\Resources\config;
+
+use Cocur\Slugify\SlugifyInterface;
+use Silverback\ApiComponentBundle\Swagger\SwaggerDecorator;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return function (ContainerConfigurator $configurator) {
+    $services = $configurator->services();
+
+    $services
+        ->defaults()
+        ->autowire()
+        ->autoconfigure()
+        ->private()
+    ;
+
+    $services
+        ->load('Silverback\\ApiComponentBundle\\Controller\\', '../../Controller')
+        ->tag('controller.service_arguments')
+    ;
+
+    $services
+        ->load('Silverback\\ApiComponentBundle\\EntityListener\\', '../../EntityListener')
+        ->tag('doctrine.orm.entity_listener')
+    ;
+
+    $services
+        ->load('Silverback\\ApiComponentBundle\\DataProvider\\', '../../DataProvider/*')
+        ->tag('api_platform.item_data_provider', [ 'priority' => 1 ])
+        ->autoconfigure(false)
+    ;
+
+    $services
+        ->alias(SlugifyInterface::class, 'slugify')
+    ;
+
+    $services
+        ->set(SwaggerDecorator::class)
+        ->decorate('api_platform.swagger.normalizer.documentation')
+        ->args(['@App\Swagger\SwaggerDecorator.inner'])
+        ->autoconfigure(false)
+    ;
+};
