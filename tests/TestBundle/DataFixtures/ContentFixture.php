1275e7b92e22496d9322b24ea0dea0296e2c4d82
Daniel West <daniel@silverback.is>
2018-02-27 18:53:06 +0000

    Test bundle add form fixture to fully test commands (functional)

--- tests/TestBundle/DataFixtures/ContentFixture.php
+++ tests/TestBundle/DataFixtures/ContentFixture.php
@@ -5,6 +5,9 @@ namespace Silverback\ApiComponentBundle\Tests\TestBundle\DataFixtures;
 use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use Silverback\ApiComponentBundle\Factory\Entity\Component\Content\ContentFactory;
+use Silverback\ApiComponentBundle\Factory\Entity\Component\Form\FormFactory;
+use Silverback\ApiComponentBundle\Tests\TestBundle\Form\TestHandler;
+use Silverback\ApiComponentBundle\Tests\TestBundle\Form\TestType;
 
 class ContentFixture extends AbstractFixture
 {
@@ -14,14 +17,20 @@ class ContentFixture extends AbstractFixture
      * @var ContentFactory
      */
     private $contentFactory;
+    /**
+     * @var FormFactory
+     */
+    private $formFactory;
 
     public function __construct(
-        ContentFactory $contentFactory
+        ContentFactory $contentFactory,
+        FormFactory $formFactory
     ) {
         $this->contentFactory = $contentFactory;
+        $this->formFactory = $formFactory;
     }
 
-    public function load(ObjectManager $manager)
+    public function load(ObjectManager $manager): void
     {
         $content = $this->contentFactory->create(
             [
@@ -29,6 +38,13 @@ class ContentFixture extends AbstractFixture
             ]
         );
         $manager->persist($content);
+        $form = $this->formFactory->create(
+            [
+                'formType' => TestType::class,
+                'successHandler' => TestHandler::class
+            ]
+        );
+        $manager->persist($form);
         $manager->flush();
     }
 }
