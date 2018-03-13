3915047ff1e351d4ed161582c9341575372b1d21
Daniel West <daniel@silverback.is>
2018-03-13 13:03:15 +0000

    get navbar ID on first layout call

--- src/Entity/Layout/Layout.php
+++ src/Entity/Layout/Layout.php
@@ -33,7 +33,7 @@ class Layout
     /**
      * @ORM\ManyToOne(targetEntity="Silverback\ApiComponentBundle\Entity\Content\Component\Navigation\NavBar\NavBar")
      * @ORM\JoinColumn(onDelete="SET NULL")
-     * @Groups({"layout"})
+     * @Groups({"layout", "route"})
      * @var null|NavBar
      */
     private $navBar;
