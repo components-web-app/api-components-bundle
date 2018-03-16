2251ba830b2ba44bb8266aaaf70d88ea3aafc480
Daniel West <daniel@silverback.is>
2018-03-16 12:39:52 +0000

    Add static childComponentGroup for navigations

--- src/Entity/Content/Component/Navigation/AbstractNavigation.php
+++ src/Entity/Content/Component/Navigation/AbstractNavigation.php
@@ -2,7 +2,10 @@
 
 namespace Silverback\ApiComponentBundle\Entity\Content\Component\Navigation;
 
+use ApiPlatform\Core\Annotation\ApiProperty;
 use Silverback\ApiComponentBundle\Entity\Content\Component\AbstractComponent;
+use Silverback\ApiComponentBundle\Entity\Content\ComponentGroup;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
  * Class AbstractNavigation
@@ -10,4 +13,26 @@ use Silverback\ApiComponentBundle\Entity\Content\Component\AbstractComponent;
  * @author Daniel West <daniel@silverback.is>
  */
 abstract class AbstractNavigation extends AbstractComponent
-{}
+{
+    /**
+     * @ORM\ManyToOne(targetEntity="Silverback\ApiComponentBundle\Entity\Content\ComponentGroup", cascade={"persist"})
+     * @ApiProperty(attributes={"fetchEager": false})
+     * @Groups({"layout", "route", "content"})
+     * @var ComponentGroup
+     */
+    protected $childComponentGroup;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->childComponentGroup = new ComponentGroup();
+    }
+
+    /**
+     * @return ComponentGroup
+     */
+    public function getChildComponentGroup(): ComponentGroup
+    {
+        return $this->childComponentGroup;
+    }
+}
