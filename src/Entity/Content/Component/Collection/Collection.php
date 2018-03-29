8f243f8ada71793fc4056a4035d6b478777b21f3
Daniel West <daniel@silverback.is>
2018-03-29 18:15:03 +0100

    Test suite configure security providers

---
+++ src/Entity/Content/Component/Collection/Collection.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Silverback\ApiComponentBundle\Entity\Content\Component\Collection;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Annotation\ApiSubresource;
+use Silverback\ApiComponentBundle\Entity\Content\Component\AbstractComponent;
+
+/**
+ * @ApiResource()
+ * @author Daniel West <daniel@silverback.is>
+ */
+class Collection extends AbstractComponent
+{
+    /**
+     * @ApiSubresource()
+     * @var mixed
+     */
+    private $resource;
+
+    /**
+     * @return mixed
+     */
+    public function getResource()
+    {
+        return $this->resource;
+    }
+}
