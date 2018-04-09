1a6071ce1c39151ae1595ae0de68a01d5beadbce
Daniel West <daniel@silverback.is>
2018-04-09 16:21:02 +0100

    Fix typo

--- src/Factory/Entity/Content/Component/AbstractComponentFactory.php
+++ src/Factory/Entity/Content/Component/AbstractComponentFactory.php
@@ -32,7 +32,7 @@ abstract class AbstractComponentFactory extends AbstractFactory
             $this->manager->persist($location);
         }
         if (
-            $this->ops['dynamicPage']
+            $this->ops['dynamicPageClass']
         ) {
             $location = new ComponentLocation(null, $component);
             $location->setDynamicPageClass($this->ops['dynamicPageClass']);
