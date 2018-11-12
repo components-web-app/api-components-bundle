f6d1905300a8e1b980ea71a21dd69c3b45b9aa73
Daniel West <daniel@silverback.is>
2018-11-12 16:58:22 +0000

    Cascade route persist from navbaritem

--- src/Entity/Component/Navigation/AbstractNavigationItem.php
+++ src/Entity/Component/Navigation/AbstractNavigationItem.php
@@ -22,7 +22,7 @@ abstract class AbstractNavigationItem extends AbstractComponent implements Navig
     protected $label;
 
     /**
-     * @ORM\ManyToOne(targetEntity="Silverback\ApiComponentBundle\Entity\Route\Route")
+     * @ORM\ManyToOne(targetEntity="Silverback\ApiComponentBundle\Entity\Route\Route", cascade={"persist"})
      * @ORM\JoinColumn(referencedColumnName="route", onDelete="CASCADE")
      * @Groups({"layout", "content", "component"})
      * @var null|Route
