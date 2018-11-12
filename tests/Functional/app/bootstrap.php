e6f82b5854d9b729866de419b419a030d333858c
Daniel West <daniel@silverback.is>
2018-11-12 16:44:11 +0000

    Update CI env vars for latest phpunit with sf bridge

--- tests/Functional/app/bootstrap.php
+++ tests/Functional/app/bootstrap.php
@@ -4,7 +4,9 @@ date_default_timezone_set('UTC');
 
 require __DIR__ . '/../../../vendor/autoload.php';
 // For behat tests to use php assertion
-if (file_exists(__DIR__ . '/../../../vendor/bin/.phpunit/phpunit-7.4/vendor/autoload.php')) {
-    require __DIR__ . '/../../../vendor/bin/.phpunit/phpunit-7.4/vendor/autoload.php';
+const PHPUNIT_AUTOLOAD = __DIR__ . '/../../../vendor/bin/.phpunit/phpunit-7.4/vendor/autoload.php';
+if (file_exists(PHPUNIT_AUTOLOAD)) {
+    require PHPUNIT_AUTOLOAD;
 }
+
 require __DIR__ . '/AppKernel.php';
