c0191002d0dd893fb498b307c477a6e4b5a9b722
Daniel West <daniel@silverback.is>
2018-11-12 16:34:50 +0000

    Use phpunit from symfony bridge in behat

--- tests/Functional/app/bootstrap.php
+++ tests/Functional/app/bootstrap.php
@@ -1,6 +1,11 @@
 <?php
 
 date_default_timezone_set('UTC');
+const PHPUNIT_AUTOLOAD = __DIR__ . '/../../../vendor/bin/.phpunit/phpunit-7.4/vendor/autoload.php';
 
 require __DIR__ . '/../../../vendor/autoload.php';
+// For behat tests to use php assertion
+if (file_exists(PHPUNIT_AUTOLOAD)) {
+    require PHPUNIT_AUTOLOAD;
+}
 require __DIR__ . '/AppKernel.php';
