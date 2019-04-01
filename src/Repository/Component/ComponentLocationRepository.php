29d11acfccf91f87e1464413f278c45661eac51f
Daniel West <daniel@silverback.is>
2019-04-02 00:33:13 +0100

    Remove unused repository method

--- src/Repository/Component/ComponentLocationRepository.php
+++ src/Repository/Component/ComponentLocationRepository.php
@@ -5,8 +5,6 @@ declare(strict_types=1);
 namespace Silverback\ApiComponentBundle\Repository\Component;
 
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Silverback\ApiComponentBundle\Entity\Component\ComponentLocation;
 use Symfony\Bridge\Doctrine\RegistryInterface;
 
@@ -21,27 +19,5 @@ class ComponentLocationRepository extends ServiceEntityRepository
     public function __construct(RegistryInterface $registry)
     {
         parent::__construct($registry, ComponentLocation::class);
-    }
-
-    public function findByDynamicPage(string $dynamicPageClass): Collection
-    {
-        $qb = $this->createQueryBuilder('location');
-        $qb
-            ->andWhere(
-                $qb->expr()->eq('location.dynamicPageClass', ':cls')
-            )
-            ->setParameter('cls', $dynamicPageClass)
-            ->addOrderBy('location.sort', 'ASC');
-
-        $result = new ArrayCollection($qb->getQuery()->getResult());
-
-        $uow = $this->getEntityManager()->getUnitOfWork();
-        $scheduledInsertions = $uow->getScheduledEntityInsertions();
-        foreach ($scheduledInsertions as $scheduledInsertion) {
-            if ($scheduledInsertion instanceof ComponentLocation && $scheduledInsertion->getDynamicPageClass() === $dynamicPageClass) {
-                $result->add($scheduledInsertion);
-            }
-        }
-        return $result;
     }
 }
