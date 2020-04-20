0694ed9b222b67234a6715c810019bcb1d4fce7e
PierreReb <pierre@les-tilleuls.coop>
2020-04-20 16:05:50 +0200

    Add two firsts tests

--- features/bootstrap/PublishableContext.php
+++ features/bootstrap/PublishableContext.php
@@ -16,6 +16,9 @@ namespace Silverback\ApiComponentBundle\Features\Bootstrap;
 use Behat\Behat\Context\Context;
 use Behat\Behat\Hook\Scope\BeforeScenarioScope;
 use Behatch\Context\JsonContext as BehatchJsonContext;
+use Behatch\Context\RestContext as BehatchRestContext;
+use Doctrine\Persistence\ManagerRegistry;
+use Silverback\ApiComponentBundle\Tests\Functional\TestBundle\Entity\DraftComponent;
 
 /**
  * @author Pierre Rebeilleau <pierre@les-tilleuls.coop>
@@ -23,32 +26,56 @@ use Behatch\Context\JsonContext as BehatchJsonContext;
 final class PublishableContext implements Context
 {
     private DoctrineContext $doctrineContext;
+    private BehatchRestContext $behatchRestContext;
     private ?BehatchJsonContext $behatchJsonContext;
-    private JsonContext $jsonContext;
+    private ManagerRegistry $doctrine;
 
-    public function __construct(DoctrineContext $doctrineContext)
+    public function __construct(DoctrineContext $doctrineContext, ManagerRegistry $doctrine)
     {
         $this->doctrineContext = $doctrineContext;
+        $this->doctrine = $doctrine;
     }
+
     /**
      * @BeforeScenario
      */
     public function gatherContexts(BeforeScenarioScope $scope): void
     {
         $this->behatchJsonContext = $scope->getEnvironment()->getContext(BehatchJsonContext::class);
+        $this->behatchRestContext = $scope->getEnvironment()->getContext(BehatchRestContext::class);
     }
 
     /**
      * @When I get a collection of published resources with draft resources available
      */
-    public function toto(): void{
-        // TODO Create draft components objects with draft resources available
-        // TODO Send a get request to '/draft_components'
+    public function iGetACollectionOfPublishedResourcesWithDraftResourcesAvailable(): void
+    {
+        $objects = [];
+
+        for ($i = 0; $i < 5; ++$i) {
+            $object = new DraftComponent();
+            $object->name = "toto $i";
+            $object->setPublishedAt(new \DateTime());
+            $this->manager->persist($object);
+            $this->manager->flush();
+            $object[$i] = $object;
+        }
+        for ($i = 0; $i < 2; ++$i) {
+            $object = new DraftComponent();
+            $object->name = "toto $i";
+            $object->setPublishedRessource($objects[$i]);
+            $this->manager->persist($object);
+            $this->manager->flush();
+        }
+
+        $this->behatchRestContext->iSendARequestTo('GET', '/draft_components');
     }
+
     /**
      * @Then it should include the draft resources instead of the published ones
      */
-    public function tata(): void{
-        $this->behatchJsonContext->theJsonShouldBeValidAccordingToTheSchema(__DIR__.'/../schema/draft.schema.json');
+    public function itShouldInculeTheDraftResourcesInsteadOfThePublishedOnes(): void
+    {
+        $this->behatchJsonContext->theJsonShouldBeValidAccordingToTheSchema(__DIR__ . '/../schema/draft.schema.json');
     }
 }
