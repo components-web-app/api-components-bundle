84c996a3ee76b30afa59b8a0412cd50fcd409f7e
Daniel West <daniel@silverback.is>
2020-04-20 12:23:54 +0100

    Re-introduce coverage for Behat

---
+++ tests/Validator/Constraints/NewEmailAddressTest.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * This file is part of the Silverback API Component Bundle Project
+ *
+ * (c) Daniel West <daniel@silverback.is>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Silverback\ApiComponentBundle\Tests\Validator\Constraints;
+
+use PHPUnit\Framework\TestCase;
+use Silverback\ApiComponentBundle\Validator\Constraints\NewEmailAddress;
+
+class NewEmailAddressTest extends TestCase
+{
+    public function test_new_email_address_is_class_constraint(): void
+    {
+        $newEmailAddressConstraint = new NewEmailAddress();
+        $this->assertEquals($newEmailAddressConstraint::CLASS_CONSTRAINT, $newEmailAddressConstraint->getTargets());
+    }
+}
