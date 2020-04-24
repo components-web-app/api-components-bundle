e17a45cf1f808b3bdb76a51e563b430b190fbc95
Daniel West <daniel@silverback.is>
2020-04-24 10:19:01 +0100

    Remove doctrine migrations bundle from test bundle - config error caused, also unnecessary

--- tests/Functional/app/config/bundles.php
+++ tests/Functional/app/config/bundles.php
@@ -20,7 +20,6 @@ return [
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
-    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
