4d5bf01d20fae66a7b407d573edc9dffd0e31497
Daniel West <daniel@silverback.is>
2020-04-28 00:05:09 +0100

    Behat scenarios for timestamped annotation

---
+++ tests/Functional/TestBundle/Entity/DummyCustomTimestamped.php
@@ -0,0 +1,68 @@
+<?php
+
+/*
+ * This file is part of the Silverback API Component Bundle Project
+ *
+ * (c) Daniel West <daniel@silverback.is>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Silverback\ApiComponentBundle\Tests\Functional\TestBundle\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use DateTime;
+use DateTimeImmutable;
+use Doctrine\ORM\Mapping as ORM;
+use Silverback\ApiComponentBundle\Annotation as Silverback;
+use Silverback\ApiComponentBundle\Entity\Utility\IdTrait;
+
+/**
+ * @author Daniel West <daniel@silverback.is>
+ *
+ * @Silverback\Timestamped(createdAtField="customCreatedAt", modifiedAtField="customModifiedAt")
+ * @ApiResource
+ * @ORM\Entity
+ */
+class DummyCustomTimestamped
+{
+    use IdTrait;
+
+    private ?DateTimeImmutable $customCreatedAt = null;
+
+    public ?DateTime $customModifiedAt = null;
+
+    public function __construct()
+    {
+        $this->setId();
+    }
+
+    public function setCustomCreatedAt(DateTimeImmutable $customCreatedAt): self
+    {
+        if (!$this->customCreatedAt) {
+            $this->customCreatedAt = $customCreatedAt;
+        }
+
+        return $this;
+    }
+
+    public function getCustomCreatedAt(): ?DateTimeImmutable
+    {
+        return $this->customCreatedAt;
+    }
+
+    public function setCustomModifiedAt(DateTime $customModifiedAt): self
+    {
+        $this->customModifiedAt = $customModifiedAt;
+
+        return $this;
+    }
+
+    public function getCustomModifiedAt(): ?DateTime
+    {
+        return $this->customModifiedAt;
+    }
+}
