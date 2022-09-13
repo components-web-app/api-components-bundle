7de387e42a2fbca83689267e8e662c206c6640c6
silverbackdan <daniel@silverback.is>
2022-09-13 15:48:43 +0100

    Complete and remove todos

--- src/DependencyInjection/CompilerPass/ApiPlatformCompilerPass.php
+++ src/DependencyInjection/CompilerPass/ApiPlatformCompilerPass.php
@@ -13,14 +13,10 @@ declare(strict_types=1);
 
 namespace Silverback\ApiComponentsBundle\DependencyInjection\CompilerPass;
 
-use GuzzleHttp\Client;
 use Silverback\ApiComponentsBundle\EventListener\Api\CollectionApiEventListener;
 use Silverback\ApiComponentsBundle\EventListener\Doctrine\PurgeHttpCacheListener;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Definition;
-use Symfony\Component\DependencyInjection\Reference;
-use Symfony\Component\HttpClient\ScopingHttpClient;
 
 /**
  * @author Daniel West <daniel@silverback.is>
@@ -33,24 +29,8 @@ class ApiPlatformCompilerPass implements CompilerPassInterface
 
         $container->getDefinition(CollectionApiEventListener::class)->setArgument('$itemsPerPageParameterName', $itemsPerPageParameterName);
 
-        // Todo: revert to checking for service id api_platform.http_cache.purger when https://github.com/api-platform/core/pull/4695 is merged
-        if (!$container->hasDefinition('api_platform.http_cache.purger.varnish.xkey')) {
+        if (!$container->hasDefinition('api_platform.http_cache.purger')) {
             $container->removeDefinition(PurgeHttpCacheListener::class);
-
-            return;
         }
-
-        // Todo: remove this - API Platform will be implementing this shortly using http client instead of guzzle
-        $config = $container->getExtensionConfig('api_platform');
-        $apiPlatformConfig = array_merge(...$config);
-        $definitions = [];
-        foreach ($apiPlatformConfig['http_cache']['invalidation']['varnish_urls'] as $key => $url) {
-            $ops = $apiPlatformConfig['http_cache']['invalidation']['request_options'] ?? [];
-            $definition = new Definition(ScopingHttpClient::class, [new Reference('http_client'), $url, ['base_uri' => $url] + $ops]);
-            $definition->setFactory([ScopingHttpClient::class, 'forBaseUri']);
-
-            $definitions[] = $definition;
-        }
-        $container->findDefinition('api_platform.http_cache.purger.varnish.xkey')->setArgument('$clients', $definitions);
     }
 }
--- src/DependencyInjection/SilverbackApiComponentsExtension.php
+++ src/DependencyInjection/SilverbackApiComponentsExtension.php
@@ -237,11 +237,9 @@ class SilverbackApiComponentsExtension extends Extension implements PrependExten
         $loader->load('services.php');
         $loader->load('services_normalizers.php');
 
-        // Todo: revert to checking for service id api_platform.http_cache.purger when https://github.com/api-platform/core/pull/4695 is merged
-        // dump($container->hasDefinition('api_platform.http_cache.purger.varnish.xkey'));
-        // if ($container->hasDefinition('api_platform.http_cache.purger.varnish.xkey')) {
-        $loader->load('services_doctrine_orm_http_cache_purger.php');
-        // }
+        if ($container->hasDefinition('api_platform.http_cache.purger')) {
+            $loader->load('services_doctrine_orm_http_cache_purger.php');
+        }
     }
 
     public function prepend(ContainerBuilder $container): void
