d3bed3f6f6960fe2aa64f60ed79d6bd3f29eff15
soyuka <soyuka@users.noreply.github.com>
2022-10-19 17:35:12 +0200

    fix(mercure): mercure subcription on published components

---
+++ src/ApiPlatform/Api/PublishableIriConverter.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+ * This file is part of the Silverback API Components Bundle Project
+ *
+ * (c) Daniel West <daniel@silverback.is>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Silverback\ApiComponentsBundle\ApiPlatform\Api;
+
+use ApiPlatform\Api\IriConverterInterface;
+use ApiPlatform\Api\UrlGeneratorInterface;
+use ApiPlatform\Metadata\Operation;
+use Silverback\ApiComponentsBundle\Helper\Publishable\PublishableStatusChecker;
+
+/**
+ * @author Daniel West <daniel@silverback.is>
+ */
+class PublishableIriConverter implements IriConverterInterface
+{
+    public function __construct(private IriConverterInterface $decorated, private PublishableStatusChecker $publishableStatusChecker)
+    {
+    }
+
+    public function getResourceFromIri(string $iri, array $context = [], ?Operation $operation = null): object
+    {
+        return $this->decorated->getResourceFromIri($iri, $context, $operation);
+    }
+
+    public function getIriFromResource($resource, int $referenceType = UrlGeneratorInterface::ABS_PATH, ?Operation $operation = null, array $context = []): ?string
+    {
+        $iri = $this->decorated->getIriFromResource($resource, $referenceType, $operation, $context);
+
+        if (\is_string($resource)) {
+            return $iri;
+        }
+
+        if ($this->publishableStatusChecker->getAnnotationReader()->isConfigured($resource) && !$this->publishableStatusChecker->isActivePublishedAt($resource)) {
+            $iri .= '?draft=1';
+        }
+
+        return $iri;
+    }
+}
