63c285c0d9e00a44877b68c4a88cb0552b550a33
silverbackdan <daniel@silverback.is>
2023-05-08 13:44:05 +0100

    Add APIP conflicts

--- tests/Functional/app/config/bundles.php
+++ tests/Functional/app/config/bundles.php
@@ -1,16 +1,5 @@
 <?php
 
-/*
- * This file is part of the Silverback API Components Bundle Project
- *
- * (c) Daniel West <daniel@silverback.is>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
-declare(strict_types=1);
-
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
