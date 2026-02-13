e9a0dc41189609359f6bee3ba3f60e1727512fba
silverbackdan <daniel@silverback.is>
2026-02-13 13:56:58 +0000

    Support only 7.4

--- src/Resources/config/routing/all.php
+++ src/Resources/config/routing/all.php
@@ -1,5 +1,16 @@
 <?php
 
+/*
+ * This file is part of the Silverback API Components Bundle Project
+ *
+ * (c) Daniel West <daniel@silverback.is>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
 namespace Symfony\Component\Routing\Loader\Configurator;
 
 use Silverback\ApiComponentsBundle\Action\User\EmailAddressConfirmAction;
@@ -16,38 +27,38 @@ $routes->add('api_components_login_check', new Route('/login'));
 $routes->add('api_components_logout', new Route('/logout'));
 $routes->add('api_components_password_reset_request', new Route('/password/reset/request/{username}', [
     [
-        '_controller' => PasswordRequestAction::class
-    ]
+        '_controller' => PasswordRequestAction::class,
+    ],
 ]));
 
 $confirmEmailRoute = new Route('/confirm-email/{username}/{emailAddress}/{token}', [
     [
-        '_controller' => EmailAddressConfirmAction::class
-    ]
+        '_controller' => EmailAddressConfirmAction::class,
+    ],
 ]);
 $confirmEmailRoute->setMethods(['GET']);
 $routes->add('api_components_confirm_email', $confirmEmailRoute);
 
 $verifyEmailRoute = new Route('/verify-email/{username}/{token}', [
     [
-        '_controller' => VerifyEmailAddressAction::class
-    ]
+        '_controller' => VerifyEmailAddressAction::class,
+    ],
 ]);
 $confirmEmailRoute->setMethods(['GET']);
 $routes->add('api_components_verify_email', $verifyEmailRoute);
 
 $resendVerificationRoute = new Route('/verify-email/{username}/{token}', [
     [
-        '_controller' => ResendVerifyEmailAddressAction::class
-    ]
+        '_controller' => ResendVerifyEmailAddressAction::class,
+    ],
 ]);
 $confirmEmailRoute->setMethods(['GET']);
 $routes->add('api_components_resend_email_verification', $resendVerificationRoute);
 
 $resendNewEmailRoute = new Route('/resend-verify-new-email/{username}', [
     [
-        '_controller' => ResendVerifyNewEmailAddressAction::class
-    ]
+        '_controller' => ResendVerifyNewEmailAddressAction::class,
+    ],
 ]);
 $confirmEmailRoute->setMethods(['GET']);
 $routes->add('api_components_resend_new_email_verification', $resendNewEmailRoute);
