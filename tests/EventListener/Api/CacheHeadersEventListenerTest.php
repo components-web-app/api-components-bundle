b99c1d8c4bfa44f0a22a4043b22707370dcfdf00
silverbackdan <daniel@silverback.is>
2026-07-17 21:15:34 +0100

    CS Fix

--- tests/EventListener/Api/CacheHeadersEventListenerTest.php
+++ tests/EventListener/Api/CacheHeadersEventListenerTest.php
@@ -21,8 +21,8 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\ResponseEvent;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
-use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
