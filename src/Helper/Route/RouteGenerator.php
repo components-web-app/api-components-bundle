048c1d7ce948710755c3e767ecf991c75b143be9
silverbackdan <daniel@silverback.is>
2026-09-26 11:01:45 +0100

    Revert "TEMP: reorder two independent setters to exercise the changed-lines Infection run"
    This reverts commit 4b46e7b4b126e75087904aa22d9b8ba2d1624a5f.

--- src/Helper/Route/RouteGenerator.php
+++ src/Helper/Route/RouteGenerator.php
@@ -86,8 +86,8 @@ class RouteGenerator implements RouteGeneratorInterface
 
         if ($existingRoute) {
             $existingRoute->setRedirect($route);
-            $route->setpageData($existingRoute->getPageData());
             $route->setPage($existingRoute->getPage());
+            $route->setpageData($existingRoute->getPageData());
         }
 
         return $route;
